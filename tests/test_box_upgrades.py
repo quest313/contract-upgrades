e3a635be75d6079e5111d50d2cb2ba41f9f1ef7c
Ben Sleek <benjaminsleek@gmail.com>
2022-01-06 19:31:51 -0500

    Initial Commit

---
+++ tests/test_box_upgrades.py
@@ -0,0 +1,35 @@
+from scripts.util import encode_function_data, get_account, upgrade
+from brownie import (
+    Box,
+    ProxyAdmin,
+    TransparentUpgradeableProxy,
+    BoxV2,
+    Contract,
+    exceptions,
+)
+import pytest
+
+
+def test_proxy_upgrades():
+    account = get_account()
+    box = Box.deploy({"from": account})
+    proxy_admin = ProxyAdmin.deploy({"from": account})
+    box_enabled_initializer_fn = encode_function_data()
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address,
+        proxy_admin.address,
+        box_enabled_initializer_fn,
+        {"from": account, "gas_limit": 1000000},
+    )
+
+    # deploy box v2
+    box_v2 = BoxV2.deploy({"from": account})
+    proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
+    with pytest.raises(exceptions.VirtualMachineError):
+        proxy_box.increment({"from": account})
+
+    upgrade(account, proxy, box_v2.address, proxy_admin_contract=proxy_admin)
+
+    assert proxy_box.retrieve() == 0
+    proxy_box.increment({"from": account})
+    assert proxy_box.retrieve() == 1
